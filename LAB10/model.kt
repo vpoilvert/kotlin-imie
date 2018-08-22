c48f6d290ff5b177a93adbe8aa0fc419af94880e
Vincent Poilvert <v.poilvert@gmail.com>
2018-08-22 14:39:41 +0200

    Add LAB10

---
+++ LAB10/model.kt
@@ -0,0 +1,22 @@
+package com.zenika.tournamentgenerator.model
+
+data class Player(val pseudo: String)
+data class Match(val player1: Player, val player2: Player)
+data class Round(val matches: List<Match>)
+
+data class Tournament(val name: String, val players: List<Player>) {
+    init {
+        if(players.size !in 2..32 || (players.size and players.size - 1 != 0)) {
+            throw IllegalArgumentException()
+        }
+
+        when {
+            players.size !in 2..32 -> throw IllegalArgumentException("Number of players must ")
+            players.size and players.size - 1 != 0 -> throw IllegalArgumentException("")
+        }
+    }
+}
+
+enum class RoundName {
+    FINAL, SEMI_FINALS, QUARTER_FINALS, HEIGHTH_FINALS, SIXTEENTH_FINALS
+}
